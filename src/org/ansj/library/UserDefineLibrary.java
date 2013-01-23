9aa663ad2e64080539898c9b9204753b5c8b433b
ansj <ansj-sun@163.com>
2013-01-24 00:00:55 +0800

    用户自定义词典操作类.提供了清空词典.加入词典的功能

--- src/org/ansj/library/UserDefineLibrary.java
+++ src/org/ansj/library/UserDefineLibrary.java
@@ -17,6 +17,10 @@ import love.cq.util.StringUtil;
 
 import org.ansj.util.MyStaticValue;
 
+/**
+ * 用户自定义词典操作类
+ * @author ansj
+ */
 public class UserDefineLibrary {
 	public static Forest FOREST = null;
 	private static final String[] PARAMER = { "userDefine", "1000" };
@@ -28,51 +32,37 @@ public class UserDefineLibrary {
 		GetWord word = FOREST.getWord("达到");
 
 		System.out.println(word.getFrontWords());
+		UserDefineLibrary.clear() ;
+		
+		word = FOREST.getWord("达到");
+		
+		System.out.println(word.getFrontWords());
 	}
 
 	static {
 		String temp = null;
+		BufferedReader br = null;
 		try {
 			long start = System.currentTimeMillis();
 			FOREST = new Forest();
 			// 先加载系统内置补充词典
-			BufferedReader br = MyStaticValue.getSystemLibraryReader();
+			br = MyStaticValue.getSystemLibraryReader();
 
 			while ((temp = br.readLine()) != null) {
-				if (StringUtil.isBlank(temp) || InitDictionary.isInSystemDic(temp.split("\t")[0])) {
+				if (StringUtil.isBlank(temp)) {
 					continue;
 				} else {
 					Library.insertWord(FOREST, temp);
 				}
 			}
-			// 如果系统设置了用户词典.那么..呵呵
-			temp = MyStaticValue.userDefinePath;
-			// 加载用户自定义词典
-			File file = null;
-			if ((temp != null || (temp = MyStaticValue.rb.getString("userLibrary")) != null)) {
-				file = new File(temp);
-				if (file.isFile()) {
-					loadFile(file);
-				} else if (file.isDirectory()) {
-					File[] files = file.listFiles();
-					for (int i = 0; i < files.length; i++) {
-						if (file.getName().trim().endsWith(".dic")) {
-							loadFile(files[i]);
-						}
-					}
-				} else {
-					System.err.println("init user library  error :" + temp + " because : not find that file !");
-				}
-
-			} else {
-				System.err.println("init user library  error :" + temp + " because : not find that file !");
-			}
-
+			loadLibrary(MyStaticValue.userDefinePath);
 			System.out.println("init user library ok use time :" + (System.currentTimeMillis() - start));
 		} catch (Exception e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 			System.err.println("init user library error :" + temp);
+		} finally {
+			IOUtil.close(br);
 		}
 	}
 
@@ -127,18 +117,45 @@ public class UserDefineLibrary {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		} finally {
-			if (br != null) {
-				IOUtil.close(br);
-			}
+			IOUtil.close(br);
 			br = null;
+		}
+	}
+
+	/**
+	 * 加载词典,传入一本词典的路径.或者目录.词典后缀必须为.dic
+	 */
+	public static void loadLibrary(String temp) {
+		// 加载用户自定义词典
+		File file = null;
+		if ((temp != null || (temp = MyStaticValue.rb.getString("userLibrary")) != null)) {
+			file = new File(temp);
+			if (file.isFile()) {
+				loadFile(file);
+			} else if (file.isDirectory()) {
+				File[] files = file.listFiles();
+				for (int i = 0; i < files.length; i++) {
+					if (file.getName().trim().endsWith(".dic")) {
+						loadFile(files[i]);
+					}
+				}
+			} else {
+				System.err.println("init user library  error :" + temp + " because : not find that file !");
+			}
 		}
 	}
 
 	/**
 	 * 删除关键词
 	 */
-
 	public static void removeWord(String word) {
 		Library.removeWord(FOREST, word);
+	}
+
+	/**
+	 * 将用户自定义词典清空
+	 */
+	public static void clear() {
+		FOREST.clear();
 	}
 }
