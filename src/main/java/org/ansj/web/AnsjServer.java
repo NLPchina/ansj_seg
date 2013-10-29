aa68d789593c4c354498712e3ed5a2383c202c63
ansj <ansj-sun@163.com>
2013-10-29 22:47:43 +0800

    update ansj web server not use jetty

--- src/main/java/org/ansj/web/AnsjServer.java
+++ src/main/java/org/ansj/web/AnsjServer.java
@@ -1,76 +1,154 @@
 package org.ansj.web;
 
-import javax.servlet.Servlet;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.UnsupportedEncodingException;
+import java.net.InetSocketAddress;
+import java.net.URI;
+import java.net.URLDecoder;
+import java.util.HashMap;
+import java.util.Map;
 
-import org.ansj.splitWord.analysis.BaseAnalysis;
-import org.ansj.splitWord.analysis.NlpAnalysis;
+import love.cq.util.IOUtil;
+import love.cq.util.StringUtil;
+
 import org.ansj.splitWord.analysis.ToAnalysis;
 import org.ansj.util.MyStaticValue;
-import org.eclipse.jetty.server.Handler;
-import org.eclipse.jetty.server.Server;
-import org.eclipse.jetty.server.handler.ContextHandlerCollection;
-import org.eclipse.jetty.server.handler.HandlerCollection;
-import org.eclipse.jetty.server.nio.SelectChannelConnector;
-import org.eclipse.jetty.servlet.ServletContextHandler;
-import org.eclipse.jetty.servlet.ServletHolder;
-import org.eclipse.jetty.util.thread.QueuedThreadPool;
 
+import com.sun.net.httpserver.HttpExchange;
+import com.sun.net.httpserver.HttpHandler;
+import com.sun.net.httpserver.HttpServer;
+import com.sun.net.httpserver.spi.HttpServerProvider;
+
+/**
+ * http 服务类
+ * 
+ * @author ansj
+ * 
+ */
 public class AnsjServer {
 
-    private final static String WARM_UP_WORD = "你好";
+	private final static String WARM_UP_WORD = "你好";
 
-    private final static int SERVER_MAX_THREADS = 100;
+	private static final String FILE_ENCODING = System.getProperty("file.encoding");
 
-    private final static int SERVER_MIN_THREADS = 10;
+	public void startServer(int serverPort) throws Exception {
+		MyStaticValue.LIBRARYLOG.info("starting ansj http server");
+		HttpServerProvider provider = HttpServerProvider.provider();
+		HttpServer httpserver = provider.createHttpServer(new InetSocketAddress(serverPort), 100);// 监听端口6666,能同时接
+																									// 受100个请求
+		httpserver.createContext("/", new AnsjHttpHandler());
+		httpserver.setExecutor(null);
+		httpserver.start();
+		System.out.println("server started");
 
-    public void startServer(int serverPort) throws Exception {
-        MyStaticValue.LIBRARYLOG.info("starting ansj http server");
+	}
 
-        /* setup server thread pool */
-        Server server = new Server();
-        QueuedThreadPool threadPool = new QueuedThreadPool();
-        threadPool.setMaxThreads(SERVER_MAX_THREADS);
-        threadPool.setMinThreads(SERVER_MIN_THREADS);
-        threadPool.setName("AnsjHttpServer");
-        server.setThreadPool(threadPool);
+	private static class AnsjHttpHandler implements HttpHandler {
+		public void handle(HttpExchange httpExchange) {
+			try {
+				String responseMsg = "欢迎使用Ansj中文分词!\ndemo:http://localhost:8888/?input=我日世界&method=to"; // 响应信息
+				Map<String, String> paramers = parseParamers(httpExchange);
+				String input = paramers.get("input");
+				String method = paramers.get("method");
+				if (StringUtil.isNotBlank(input)) {
+					responseMsg = AnsjServlet.processRequest(input, method);
+				}
 
-        /* setup channel connector */
-        SelectChannelConnector connector = new SelectChannelConnector();
-        connector.setPort(serverPort);
-        server.addConnector(connector);
+				writeToClient(httpExchange, responseMsg);
 
-        /* setup servlet and context */
-        ServletHolder servletHolder = new ServletHolder(
-                (Servlet) new AnsjServlet());
-        ContextHandlerCollection contexts = new ContextHandlerCollection();
-        ServletContextHandler context = new ServletContextHandler(contexts, "");
-        context.addServlet(servletHolder, "/segment");
+			} catch (Exception e) {
+				// TODO: handle exception
+				e.printStackTrace();
+				try {
+					writeToClient(httpExchange, e.getMessage());
+				} catch (IOException e1) {
+					// TODO Auto-generated catch block
+					e1.printStackTrace();
+				}
 
-        /* setup handler */
-        HandlerCollection handlers = new HandlerCollection();
-        handlers.setHandlers(new Handler[] {
-            contexts
-        });
-        server.setHandler(handlers);
+			} finally {
+				httpExchange.close();
+			}
+		}
 
-        /* start up server */
-        server.start();
-        MyStaticValue.LIBRARYLOG.info("ansj http server started");
-        server.join();
-    }
+		private void writeToClient(HttpExchange httpExchange, String responseMsg) throws IOException {
+			byte[] bytes = responseMsg.getBytes();
+			httpExchange.sendResponseHeaders(200, bytes.length); // 设置响应头属性及响应信息的长度
+			OutputStream out = httpExchange.getResponseBody(); // 获得输出流
+			out.write(bytes);
+			out.flush();
+		}
 
-    public static void main(String[] args) throws Exception {
-        if (args.length < 1) {
-            System.err.println("Usage: AnsjServer <serverPort>");
-            return;
-        }
-        /* warm up ansj engine */
-        /* FIXME: dirty hack here... */
-        ToAnalysis.parse(WARM_UP_WORD);
-        NlpAnalysis.parse(WARM_UP_WORD);
-        BaseAnalysis.parse(WARM_UP_WORD);
-        /* set up server */
-        int serverPort = Integer.valueOf(args[0]);
-        new AnsjServer().startServer(serverPort);
-    }
+		private Map<String, String> parseParamers(HttpExchange httpExchange) throws UnsupportedEncodingException, IOException {
+			// TODO Auto-generated method stub
+			BufferedReader reader = null;
+			try {
+				Map<String, String> parameters = new HashMap<String, String>();
+				URI requestedUri = httpExchange.getRequestURI();
+				String query = requestedUri.getRawQuery();
+				// get 请求解析
+				parseQuery(query, parameters);
+				// post 请求解析
+				reader = IOUtil.getReader(httpExchange.getRequestBody(), FILE_ENCODING);
+				query = IOUtil.getContent(reader).trim();
+				parseQuery(query, parameters);
+				httpExchange.setAttribute("parameters", parameters);
+				return parameters;
+			} finally {
+				if (reader != null) {
+					reader.close();
+				}
+			}
+		}
+
+		/**
+		 * 从get请求中解析参数
+		 * 
+		 * @param query
+		 * @param parameters
+		 */
+		private void parseQuery(String query, Map<String, String> parameters) {
+			// TODO Auto-generated method stub
+			if (StringUtil.isBlank(query)) {
+				return;
+			}
+			String split[] = query.split("\\?");
+			query = split[split.length - 1];
+			split = query.split("&");
+			String[] param = null;
+			String key = null;
+			String value = null;
+			for (String kv : split) {
+				try {
+					param = kv.split("=");
+					if (param.length == 2) {
+						key = URLDecoder.decode(param[0], FILE_ENCODING);
+						value = URLDecoder.decode(param[1], FILE_ENCODING);
+						parameters.put(key, value);
+					}
+				} catch (UnsupportedEncodingException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			}
+
+		}
+	}
+
+	public static void main(String[] args) throws Exception {
+		if (args.length < 1) {
+			System.err.println("Usage: AnsjServer <serverPort> so by default 8888");
+			args = new String[] { "8888" };
+		}
+		/* warm up ansj engine */
+		/* FIXME: dirty hack here... */
+		ToAnalysis.parse(WARM_UP_WORD);
+		/* set up server */
+		int serverPort = Integer.valueOf(args[0]);
+		new AnsjServer().startServer(serverPort);
+	}
 }
--- src/main/java/org/ansj/web/AnsjServlet.java
+++ src/main/java/org/ansj/web/AnsjServlet.java
@@ -3,72 +3,46 @@ package org.ansj.web;
 import java.io.IOException;
 import java.util.List;
 
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-
 import org.ansj.domain.Term;
 import org.ansj.recognition.NatureRecognition;
 import org.ansj.splitWord.analysis.BaseAnalysis;
 import org.ansj.splitWord.analysis.NlpAnalysis;
 import org.ansj.splitWord.analysis.ToAnalysis;
 
-public class AnsjServlet extends HttpServlet {
+public class AnsjServlet {
 
-    private enum AnsjMethod {
-        TO, NLP, BASE
-    }
+	private enum AnsjMethod {
+		TO, NLP, BASE
+	}
 
-    private static final long serialVersionUID = -5835898043864355527L;
+	public static String processRequest(String input, String strMethod) throws IOException {
+		AnsjMethod method = AnsjMethod.TO;
+		if (strMethod != null) {
+			method = AnsjMethod.valueOf(strMethod.toUpperCase());
+		} else {
+			method = AnsjMethod.TO;
+		}
+		List<Term> terms = null;
+		switch (method) {
+		case TO:
+			terms = ToAnalysis.parse(input);
+			break;
+		case NLP:
+			terms = NlpAnalysis.parse(input);
+			break;
+		default:
+			terms = BaseAnalysis.parse(input);
 
-    private void processRequest(HttpServletRequest req, HttpServletResponse resp)
-            throws IOException {
-        String input = req.getParameter("input");
-        String strMethod = req.getParameter("method");
-        AnsjMethod method = AnsjMethod.TO;
-        resp.setHeader("Content-Type", "text/plain; charset=utf-8");
-        if (input == null) {
-            resp.sendError(500, "Input not specified");
-            return;
-        }
-        if (strMethod != null) {
-            method = AnsjMethod.valueOf(strMethod.toUpperCase());
-        }
-        List<Term> terms = null;
-        switch (method) {
-            case TO:
-                terms = ToAnalysis.parse(input);
-                break;
-            case NLP:
-                terms = NlpAnalysis.parse(input);
-                break;
-            case BASE:
-                terms = BaseAnalysis.parse(input);
-                break;
-        }
-        if (terms == null) {
-            resp.sendError(500, "Failed to parse input");
-            return;
-        }
-        new NatureRecognition(terms).recognition();
-        for (Term term: terms) {
-            String tmp = term.getName() + "/" + term.getNatrue().natureStr
-                    + " ";
-            resp.getWriter().append(tmp);
-        }
-    }
-
-    @Override
-    protected void doGet(HttpServletRequest req, HttpServletResponse resp)
-            throws ServletException, IOException {
-        this.processRequest(req, resp);
-    }
-
-    @Override
-    protected void doPost(HttpServletRequest req, HttpServletResponse resp)
-            throws ServletException, IOException {
-        this.processRequest(req, resp);
-    }
+		}
+		if (terms == null) {
+			return "Failed to parse input";
+		}
+		new NatureRecognition(terms).recognition();
+		StringBuilder sb = new StringBuilder();
+		for (Term term : terms) {
+			sb.append(term.getName() + "/" + term.getNatrue().natureStr + " ");
+		}
+		return sb.toString();
+	}
 
 }
