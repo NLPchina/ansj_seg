aa54fde123db7254e23fd3071669648a7b6c527b
ansj <ansj-sun@163.com>
2013-12-12 14:09:16 +0800

    update test

--- src/main/java/org/ansj/util/MathUtil.java
+++ src/main/java/org/ansj/util/MathUtil.java
@@ -37,8 +37,6 @@ public class MathUtil {
 		double value = -Math.log(dSmoothingPara * frequency / (MAX_FREQUENCE + 80000) + (1 - dSmoothingPara)
 				* ((1 - dTemp) * nTwoWordsFreq / frequency + dTemp));
 
-		if (value < 0)
-			value += frequency;
 
 		if (value < 0) {
 			value += frequency;
---
+++ src/test/java/org/ansj/test/TestSigHan.java
@@ -0,0 +1,35 @@
+package org.ansj.test;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+import java.util.List;
+
+import love.cq.util.IOUtil;
+
+import org.ansj.domain.Term;
+import org.ansj.splitWord.analysis.ToAnalysis;
+
+
+public class TestSigHan {
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = IOUtil.getReader("/home/ansj/src/icwb2-data/testing/msr_test.utf8", "utf-8");
+        String temp = null;
+        FileOutputStream fos = new FileOutputStream(new File("/home/ansj/src/icwb2-data/test_segmentation.utf8"));
+        while ((temp = reader.readLine()) != null) {
+            List<Term> parse = ToAnalysis.parse(temp) ;
+            StringBuilder sb = new StringBuilder() ;
+            for (Term term : parse) {
+                sb.append(term.getName()+"\t") ;
+            }
+            fos.write(sb.toString().trim().getBytes()) ;
+            fos.write("\n".getBytes());
+        }
+        
+        fos.flush();
+        fos.close();
+    }
+}
