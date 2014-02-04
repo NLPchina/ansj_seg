1d8da5884ce2e6c3074ae7619fabfc0d0c6ecfd6
ansj <ansj-sun@163.com>
2014-02-04 20:06:00 +0800

    update

--- src/test/java/org/ansj/demo/DynamicWordDemo.java
+++ src/test/java/org/ansj/demo/DynamicWordDemo.java
@@ -16,7 +16,7 @@ public class DynamicWordDemo {
 	public static void main(String[] args) {
 		// 增加新词,中间按照'\t'隔开
 		UserDefineLibrary.insertWord("ansj中文分词", "userDefine", 1000);
-		List<Term> terms = ToAnalysis.parse("我觉得ansj中文分词是一个不错的系统!我是王婆!");
+		List<Term> terms = ToAnalysis.parse("我觉得Ansj中文分词是一个不错的系统!我是王婆!");
 		System.out.println("增加新词例子:" + terms);
 
 		// 删除词语,只能删除.用户自定义的词典.
