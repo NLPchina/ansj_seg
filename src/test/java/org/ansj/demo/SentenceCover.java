ee2799d2cf3188e3fd45a7f80fc9eaef2c730b27
ansj <ansj-sun@163.com>
2014-02-12 14:44:52 +0800

    up

---
+++ src/test/java/org/ansj/demo/SentenceCover.java
@@ -0,0 +1,14 @@
+package org.ansj.demo;
+
+import org.ansj.util.WordAlert;
+
+/**
+ * 句子标准化demo
+ * @author ansj
+ *
+ */
+public class SentenceCover {
+	public static void main(String[] args) {
+		System.out.println(WordAlert.alertStr("ａ ｚ Ａ Ｚ A Z  a z 0 ９ 0 9 ？ 『  ﹩"));
+	}
+}
--- src/test/java/org/ansj/test/TestError.java
+++ src/test/java/org/ansj/test/TestError.java
@@ -13,7 +13,7 @@ public class TestError {
 	
 	@Test
 	public void test(){
-		String str = "里奇和波什都因伤退出，内线又一次成为美国男篮的问题，以目前球队的大名单，两大内线钱德勒和勒夫出战奥运几乎可以确定。科朗吉洛接受福克斯体育的电话采访时明确表示奥运阵容已确认9人。2008年北京奥运夺冠的科比、詹姆斯、安东尼、保罗和德隆肯定亮相伦敦，2010年世锦赛上帮助球队夺冠的杜兰特、威斯布鲁克、勒夫和钱德勒也会入选奥运名单。" ;
+		String str = "n维空间F中的超平面是由方程定义的子集" ;
 		int len = 0 ;
 		LearnTool tool = new LearnTool() ;
 		List<Term> parse = NlpAnalysis.parse(str,tool);
