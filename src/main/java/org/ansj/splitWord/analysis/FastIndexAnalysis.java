6c0eb4aa8cd05136b087eb4b3f41b79a9170c4ed
ansj <ansj-sun@163.com>
2014-09-01 10:40:31 +0800

    目前速度只能到300w左右go on

--- src/main/java/org/ansj/splitWord/analysis/FastIndexAnalysis.java
+++ src/main/java/org/ansj/splitWord/analysis/FastIndexAnalysis.java
@@ -1,22 +1,68 @@
 package org.ansj.splitWord.analysis;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.Reader;
+import java.util.LinkedList;
+import java.util.List;
 
+import org.ansj.domain.Term;
+import org.ansj.splitWord.Analysis;
 import org.ansj.splitWord.impl.GetWordsImpl;
+import org.ansj.util.Graph;
+import org.nlpcn.commons.lang.util.IOUtil;
 
-public class FastIndexAnalysis {
+public class FastIndexAnalysis extends Analysis {
 
 	public static void main(String[] args) throws IOException {
-		String temp = "呵呵，我在项目里面设置了 core.autocrlf=false 在我本地解决了。win下还是要提醒其他人注意下的，跨多个平台开发容易导致提交不必要的变更";
 
-		GetWordsImpl gw = new GetWordsImpl(temp);
+		BufferedReader reader = IOUtil.getReader("/Users/ansj/Documents/temp/test.txt", "utf-8");
+		Term temp = null;
 
-		while ((temp = gw.allWords()) != null) {
-			System.out.println(temp);
+		
+		System.out.println(FastIndexAnalysis.parse("中国人民123adfsf.m,.sdfjiweu3u03  dsfds"));
+		long start = System.currentTimeMillis();
+
+		FastIndexAnalysis fia = new FastIndexAnalysis(reader);
+		long length = 0;
+
+		while ((temp = fia.next()) != null) {
+			length += temp.getName().length();
 		}
 		;
 
+		System.out.println(length * 1000L / (System.currentTimeMillis() - start));
 
+		System.out.println(System.currentTimeMillis() - start);
+
+		System.out.println(length);
+
+	}
+
+	public FastIndexAnalysis(Reader br) {
+		super.resetContent(br);
+	}
+	
+	public FastIndexAnalysis() {
+	}
+	
+	public static List<Term> parse(String str) {
+		return new FastIndexAnalysis().parseStr(str);
+	}
+
+	@Override
+	protected List<Term> getResult(Graph graph) {
+		graph.rmLittlePath();
+
+		List<Term> result = new LinkedList<Term>();
+		int length = graph.terms.length - 1;
+		for (int i = 0; i < length; i++) {
+			if (graph.terms[i] != null) {
+				result.add(graph.terms[i]);
+			}
+		}
+
+		return result;
 	}
 
 }
