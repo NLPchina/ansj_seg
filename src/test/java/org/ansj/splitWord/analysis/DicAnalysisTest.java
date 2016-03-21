5df62f2654f5fefcbba4b97fb2dd38ef4be1cf2d
ansj <ansj-sun@163.com>
2016-03-21 22:07:03 +0800

    update

--- src/test/java/org/ansj/splitWord/analysis/DicAnalysisTest.java
+++ src/test/java/org/ansj/splitWord/analysis/DicAnalysisTest.java
@@ -1,9 +1,16 @@
 package org.ansj.splitWord.analysis;
 
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.List;
 
+import org.ansj.domain.Term;
 import org.ansj.library.UserDefineLibrary;
 import org.junit.Test;
+import org.nlpcn.commons.lang.tire.domain.Value;
+import org.nlpcn.commons.lang.tire.library.Library;
+
+import junit.framework.Assert;
 
 public class DicAnalysisTest {
 
@@ -22,6 +29,38 @@ public class DicAnalysisTest {
 		System.out.println(DicAnalysis.parse("来自大学生小说网大学"));
 		
 		
+		String newWord = "爸爸去哪儿";
+		String nature = "aaaaa";
+		String str = "上海电力2012年财务报表如下怎爸爸去哪儿么办";
+		
+		//增加新词
+		UserDefineLibrary.insertWord(newWord, nature, 1000);
+		UserDefineLibrary.insertWord("上海电力", nature, 1000);
+		
+		List<Term> parse = DicAnalysis.parse(str);
+		HashMap<String, Term> hs = new HashMap<String, Term>();
+		for (Term term : parse) {
+			hs.put(term.getName(), term);
+		}
+		Assert.assertTrue(hs.containsKey(newWord));
+
+		Assert.assertEquals(hs.get(newWord).natrue().natureStr, nature);
+		
+		
+		Library.insertWord(UserDefineLibrary.FOREST, new Value("北京卡", "UserDefined", "1000"));
+	    
+		Assert.assertEquals(DicAnalysis.parse("北京卡机场服务").get(0).getName(), "北京卡");
+
+		//删除词
+		UserDefineLibrary.removeWord(newWord);
+		parse = DicAnalysis.parse(str);
+		hs = new HashMap<String, Term>();
+		for (Term term : parse) {
+			hs.put(term.getName(), term);
+		}
+
+		Assert.assertTrue(!hs.containsKey(newWord));
+
 	}
 
 }
