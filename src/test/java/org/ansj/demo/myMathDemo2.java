c97a18b0e74d526d74c28cb1d2f454e34c29eedd
xuming <507153809@qq.com>
2016-05-31 09:23:03 +0800

    算法更新

---
+++ src/test/java/org/ansj/demo/myMathDemo2.java
@@ -0,0 +1,21 @@
+package org.ansj.demo;
+
+import java.io.IOException;
+
+/**
+ *
+ * @Title: 动态规划-背包问题
+ * @Author: xuming
+ * @Description: 
+ * @date:2016/5/31 9:22
+ * @param 
+ * @return 
+ */
+public class myMathDemo2 {
+    
+    public static void main(String[] args) throws IOException {
+
+        
+    }
+
+}
