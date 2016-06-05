8312604436162b07d2c5d75d9bd68f0035a2badf
ansj <ansj-sun@163.com>
2016-06-05 21:13:00 +0800

    up

---
+++ src/test/java/org/ansj/domain/ResultTest.java
@@ -0,0 +1,35 @@
+package org.ansj.domain;
+
+import org.ansj.splitWord.analysis.ToAnalysis;
+import org.junit.Test;
+
+/**
+ * 返回结果的样式
+ * @author ansj
+ *
+ */
+public class ResultTest {
+
+	
+	@Test
+	public void toStringTest() {
+		System.out.println(ToAnalysis.parse("分词结果！").toString());
+	}
+	
+	@Test
+	public void toStringSplitTest() {
+		System.out.println(ToAnalysis.parse("分词结果！").toString("\t"));
+	}
+	
+	@Test
+	public void toStringWithOutNatureTest() {
+		System.out.println(ToAnalysis.parse("分词结果！").toStringWithOutNature());
+	}
+	
+	
+	@Test
+	public void toStringWithOutNatureSplitTest() {
+		System.out.println(ToAnalysis.parse("分词结果！").toStringWithOutNature("\t"));
+	}
+
+}
