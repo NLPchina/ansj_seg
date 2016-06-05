2c93bf78dbbbbea6eefaaec3ca4094c807e6a25a
ansj <ansj-sun@163.com>
2016-06-05 14:56:24 +0800

    update

--- src/test/java/org/ansj/test/TestError.java
+++ src/test/java/org/ansj/test/TestError.java
@@ -22,6 +22,8 @@ public class TestError {
 
 	@Test
 	public void test() throws Exception {
+		String str = "欢迎使用ansj_seg,(ansj中文分词)在这里如果你遇到什么问题都可以联系我.我一定尽我所能.帮助大家.ansj_seg更快,更准,更自由!" ;
+		 System.out.println(ToAnalysis.parse(str));
 
 //		LearnTool tool = new LearnTool();
 //		System.out.println(NlpAnalysis.parse("这次回家，我经济南下广州", tool));
