1a0b52e1a72c67f8550721040e3ca1c4c55d78cb
ansj <ansj-sun@163.com>
2016-06-05 16:47:10 +0800

    fix crfmodeltest path

--- src/test/java/org/ansj/app/crf/ModelTest.java
+++ src/test/java/org/ansj/app/crf/ModelTest.java
@@ -6,7 +6,7 @@ public class ModelTest {
 
 	@Test
 	public void test() throws Exception {
-		Model model = Model.load("ansj", "library/crf.model");
+		Model model = Model.load("ansj", "src/main/resources/crf.model");
 		System.out.println(new SplitWord(model).cut("结婚的和尚未结婚的"));
 
 		String path = "/Users/sunjian/Documents/src/CRF++-0.58/test/model.txt";
