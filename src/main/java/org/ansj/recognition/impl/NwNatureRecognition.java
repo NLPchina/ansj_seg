6e3973919f0d20cd9389e4ff03149ccb503b38c8
ansj <ansj-sun@163.com>
2016-07-19 21:10:34 +0800

    add nwnature

---
+++ src/main/java/org/ansj/recognition/impl/NwNatureRecognition.java
@@ -0,0 +1,21 @@
+package org.ansj.recognition.impl;
+
+import org.ansj.domain.Result;
+import org.ansj.recognition.Recognition;
+
+/**
+ * 新词发现nw的词性分类
+ * @author ansj
+ *
+ */
+public class NwNatureRecognition implements Recognition{
+	
+	
+
+	@Override
+	public void recognition(Result result) {
+		// TODO Auto-generated method stub
+		
+	}
+	
+}
