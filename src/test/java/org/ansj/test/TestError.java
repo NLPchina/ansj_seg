bbee435d435e0866b5e4e664dfb21db85e7836c6
孙健 <sunj@infcn.com.cn>
2017-01-14 21:00:02 +0800

    up

--- src/test/java/org/ansj/test/TestError.java
+++ src/test/java/org/ansj/test/TestError.java
@@ -148,32 +148,42 @@ public class TestError {
 		//		
 		//		System.out.println(NlpAnalysis.parse("2015年无锡市突发环境事件"));
 		
-		//dic分词诡异的bug，自定义词不起作用，非常诡异！ #398
-		MyStaticValue.isRealName = true ;
-		DicLibrary.insert(DicLibrary.DEFAULT,"英雄联盟") ;
-		DicLibrary.insert(DicLibrary.DEFAULT,"英联") ;
-		System.out.println(DicAnalysis.parse("英雄联盟"));
+//		//dic分词诡异的bug，自定义词不起作用，非常诡异！ #398
+//		MyStaticValue.isRealName = true ;
+//		DicLibrary.insert(DicLibrary.DEFAULT,"英雄联盟") ;
+//		DicLibrary.insert(DicLibrary.DEFAULT,"英联") ;
+//		System.out.println(DicAnalysis.parse("英雄联盟"));
+//		
+//		
+//		System.out.println(ToAnalysis.parse(""));
+//		System.out.println(DicAnalysis.parse(""));
+//		System.out.println(ToAnalysis.parse("大"));
+//		System.out.println(DicAnalysis.parse("大"));
+//
+//		Forest dict1 = new Forest();
+//		Library.insertWord(dict1, new Value("苹果", "userDefine", "1000"));
+//		Library.insertWord(dict1, new Value("咖啡", "userDefine", "1000"));
+//		System.out.println(DicAnalysis.parse("吃苹果喝咖啡", dict1));
+//		
+//		//#385 ansj_seg5.0.3版本，加载了默认的default.dic后，句子中带“哎咿呀”的，分词时会报错
+//		System.out.println(DicAnalysis.parse("哎咿呀"));
+//
+//		// #386
+//		Result parse = NlpAnalysis.parse("身高170 出生在1990年 人");
+//
+//		for (Term term : parse) {
+//			Assert.assertFalse(term.getName().equals(" 人"));
+//		}
 		
 		
-		System.out.println(ToAnalysis.parse(""));
-		System.out.println(DicAnalysis.parse(""));
-		System.out.println(ToAnalysis.parse("大"));
-		System.out.println(DicAnalysis.parse("大"));
-
-		Forest dict1 = new Forest();
-		Library.insertWord(dict1, new Value("苹果", "userDefine", "1000"));
-		Library.insertWord(dict1, new Value("咖啡", "userDefine", "1000"));
-		System.out.println(DicAnalysis.parse("吃苹果喝咖啡", dict1));
+		System.out.println(ToAnalysis.parse("┏玫红色玫红色玫红色",null));
 		
-		//#385 ansj_seg5.0.3版本，加载了默认的default.dic后，句子中带“哎咿呀”的，分词时会报错
-		System.out.println(DicAnalysis.parse("哎咿呀"));
-
-		// #386
-		Result parse = NlpAnalysis.parse("身高170 出生在1990年 人");
-
-		for (Term term : parse) {
-			Assert.assertFalse(term.getName().equals(" 人"));
-		}
+		//5.1.0版本dic分词加入的词无效 #409
+		
+		DicLibrary.insert(DicLibrary.DEFAULT, "琅琊榜","user",1000) ;
+		DicLibrary.insert(DicLibrary.DEFAULT, "玫红色");
+		System.out.println(ToAnalysis.parse("玫红色玫红色玫红色"));
+		System.out.println(DicAnalysis.parse("琅琊榜")) ;
 
 	}
 }
