a97a44a9f650c125e4562e018489fd839561852a
Ansj <sunj@infcn.com.cn>
2017-12-18 10:10:00 +0800

    up

--- src/main/java/org/ansj/app/summary/SummaryComputer.java
+++ src/main/java/org/ansj/app/summary/SummaryComputer.java
@@ -279,6 +279,8 @@ public class SummaryComputer {
 			sentence.score /= Math.log(sentence.value.length() + 3);
 		}
 
+		System.out.println(sentence.score+"\t"+sentence.value);
+
 		return flag;
 	}
 
