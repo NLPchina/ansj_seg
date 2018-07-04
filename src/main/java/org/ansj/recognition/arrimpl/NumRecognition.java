834b183439bcfa09146372d35a80ac1a2bc8233e
cjy <chenjinyuan87@gmail.com>
2018-07-04 16:26:11 +0800

    修复图结构

--- src/main/java/org/ansj/recognition/arrimpl/NumRecognition.java
+++ src/main/java/org/ansj/recognition/arrimpl/NumRecognition.java
@@ -124,13 +124,31 @@ public class NumRecognition implements TermArrRecognition {
 			}
 
 
-			if (quantifierRecognition) { //开启量词识别
-				to = temp.to();
-				if (to.termNatures().numAttr.isQua()) {
-					temp.setName(temp.getName() + to.getName());
-					terms[to.getOffe()] = null;
-					TermUtil.termLink(temp, to.to());
-					temp.setNature(to.termNatures().numAttr.nature);
+            if (quantifierRecognition) { //开启量词识别
+                to = temp.to();
+                if (to.termNatures().numAttr.isQua()) {
+                    temp.setName(temp.getName() + to.getName());
+                    Term origTo = terms[to.getOffe()];
+                    if (origTo.getName().length() > to.getName().length()) {//to的位置被别的词占用了
+                        //此修改基于TermUtil.insertTerm(Term[] terms, List tempList, TermNatures tns)的一个bug
+                        //假设现在有4个term，ABCD，现在要把BC合成一个词，那么这个函数执行的结果是：
+                        //terms[0]=A terms[1]=BC terms[3]=D
+                        //A->B->C->D
+                        //BC->D
+                        int end = origTo.getName().length() + to.getOffe();
+                        Term pre = to;
+                        Term next = to.to();//从to.to开始找回原有的词
+                        while (next != null && next.getOffe() < end) {
+                            terms[next.getOffe()] = next;
+                            pre = next;
+                            next = next.to();
+                        }
+                        if (next != null)
+                            TermUtil.termLink(pre, next);
+                    }
+                    terms[to.getOffe()] = null;
+                    TermUtil.termLink(temp, to.to());
+                    temp.setNature(to.termNatures().numAttr.nature);
 
 					if ("m".equals(to.termNatures().numAttr.nature.natureStr)) {
 						i = temp.getOffe() - 1 ;
